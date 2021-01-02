42a3ccf0a4a4732700ad7b0bb10091a34139ab29
aspire-t <zhijianwubu@gmail.com>
2021-01-03 00:37:21 +0800

    feat: 初始化base的状态

--- qiankun-base/src/views/Home.vue
+++ qiankun-base/src/views/Home.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="home">
     这个是qiankun-base
+    {{ num }}
+    <button type="primary" @click="add">+1</button>
   </div>
 </template>
 
 <script>
+import actions from '@/shared/actions'
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 
@@ -13,5 +16,33 @@ export default {
   // components: {
   //   HelloWorld
   // }
+  data() {
+    return {
+      num: 0,
+    }
+  },
+  mounted() {
+    this.initActions()
+  },
+  methods: {
+    initActions() {
+      // 注册一个观察者函数
+      actions.onGlobalStateChange((state, prevState) => {
+        console.log(111111111111)
+        // state: 变更后的状态; prevState: 变更前的状态
+        this.num = state.number
+        console.log('主应用观察者：number 改变前的值为 ', prevState.number)
+        console.log(
+          '主应用观察者：number发生改变，改变后的 number 的值为 ',
+          state.number
+        )
+      })
+    },
+    add() {
+      let number = this.num + 1
+      // 登录成功后，设置 token
+      actions.setGlobalState({ number })
+    },
+  },
 }
 </script>
