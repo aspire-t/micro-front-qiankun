1277a392177745d4070f98f3349b040fe4d0c4dc
aspire-t <zhijianwubu@gmail.com>
2021-01-14 23:38:40 +0800

    feat: react更新状态

--- qiankun-react/src/views/home.jsx
+++ qiankun-react/src/views/home.jsx
@@ -4,13 +4,14 @@ import actions from '../shared/actions'
 const Home = () => {
   let [number, setNumber] = useState(0)
   actions.onGlobalStateChange((state, prev) => {
-    // console.log(state, prev)
     setNumber(state.number)
   })
 
   const handleClick = () => {
-    setNumber(number + 1)
-    actions.setGlobalState({ number })
+    setNumber(() => number + 1)
+    actions.setGlobalState({
+      number: number + 1,
+    })
   }
 
   return (
